fa807cce784d8a1d10127cf2a567a383fdd68df5
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-08-10 21:40:55 +0100

    JavaScript Add

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 const api = {
-    key: "49aa53bf1b64e1bccbcac61ff2521429",
-    baseurl: "https://api.openweathermap.org/data/2.5/"
+    key: "5060a92bd4cbe633eab2b2d7ea5e3048",
+    base: "https://api.openweathermap.org/data/2.5/"
 }
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.KeyCode == 13) {
+       getResults(searchbox.value);
+    }
+}
+
+function getResult (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city')
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    
